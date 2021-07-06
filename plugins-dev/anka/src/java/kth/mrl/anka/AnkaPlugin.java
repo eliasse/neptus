63c78295336517254d537117e8ddc9086b99ef5e
Elias <eliasse@kth.se>
2021-07-06 13:46:38 +0200

    Blob radius can be set as a parameter in the Neptus->Plugin Manager...

--- plugins-dev/anka/src/java/kth/mrl/anka/AnkaPlugin.java
+++ plugins-dev/anka/src/java/kth/mrl/anka/AnkaPlugin.java
@@ -35,6 +35,9 @@ public class AnkaPlugin extends SimpleRendererInteraction{
 	int systemAddress = 0x0809;
 	Map lastMap;
 	
+	@NeptusProperty(name="Blob radius (pixels)")
+	public int blobRadius = 10;
+	
 	@Subscribe
 	public void consume(Announce msg) {
         if (msg.getSysName().equalsIgnoreCase(systemName)) {
@@ -60,23 +63,24 @@ public class AnkaPlugin extends SimpleRendererInteraction{
 		for (MapFeature feature: lastMap.getFeatures()) {
 			switch (feature.getFeatureType()) {
 			case POI:
-				System.out.println("Received POI");
-				MapPoint mappoint = feature.getFeature().firstElement();
-	            LocationType loc = new LocationType(Math.toDegrees(mappoint.getLat()),
-	                    Math.toDegrees(mappoint.getLon()));
-	            Point2D pt = renderer.getScreenPosition(loc);
-	            Ellipse2D ellis = new Ellipse2D.Double(pt.getX() - 100, pt.getY() - 100,
-	                    100 * 2, 100 * 2);
-	            
-	            float[] hsb = Color.RGBtoHSB(feature.getRgbRed(), 
-	            		feature.getRgbGreen(), 
-	            		feature.getRgbBlue(), null);
-	            
-	            Color color = Color.getHSBColor(hsb[0], hsb[1], hsb[2]);
-	            
-	            g.setColor(color);
-	            //g.draw(pt);
-	            g.fill(ellis);
+				for (MapPoint mappoint : feature.getFeature()) {
+					//MapPoint mappoint = feature.getFeature().firstElement();
+		            LocationType loc = new LocationType(Math.toDegrees(mappoint.getLat()),
+		                    Math.toDegrees(mappoint.getLon()));
+		            Point2D pt = renderer.getScreenPosition(loc);
+		            Ellipse2D ellis = new Ellipse2D.Double(pt.getX() - blobRadius, pt.getY() - blobRadius,
+		                    blobRadius * 2, blobRadius * 2);
+		            
+		            float[] hsb = Color.RGBtoHSB(feature.getRgbRed(), 
+		            		feature.getRgbGreen(), 
+		            		feature.getRgbBlue(), null);
+		            
+		            Color color = Color.getHSBColor(hsb[0], hsb[1], hsb[2]);
+		            
+		            g.setColor(color);
+		            //g.draw(pt);
+		            g.fill(ellis);
+				}
 				break;
 			case LINE:
 				System.out.println("Received LINE");
